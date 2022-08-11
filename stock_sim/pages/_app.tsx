eac49374c00452470549d083809bbeffa04b21c3
Immanuel Currah <immanueli.currah@gmail.com>
2022-08-11 10:27:44 -0400

    auth0 broke when I created an account with it not sure why will look into it more

--- stock_sim/pages/_app.tsx
+++ stock_sim/pages/_app.tsx
@@ -1,15 +1,15 @@
 import '../styles/globals.css'
-import { Auth0Provider } from '@auth0/auth0-react'
 import type { AppProps } from 'next/app'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-       <Auth0Provider
-    domain="dev-ngwp0q4r.us.auth0.com"
-    clientId="CiHidVqJCoxjAEO3Sc0e7OL9ZlVlvP5J"
-    redirectUri={"https://localhost:3000"}
-    >
+      <Auth0Provider
+        domain="dev-ngwp0q4r.us.auth0.com"
+        clientId="Xu4ywTIHaIpoyAcnwWtdDrlQjYtYRjfS"
+        redirectUri={"https://localhost:3000"}
+      >
         <Component {...pageProps} />
       </Auth0Provider>
     </>
--- stock_sim/pages/index.tsx
+++ stock_sim/pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import {Login} from "../component/Login"
+import LoginButton from '../components/Login'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Login/>
+      "hi"
+      <LoginButton/>
     </div>
   )
 }
